d7a42dad865b5e8c1fd3cf29aad5539964a208f8
Premmmas <pimiastianhi@gmail.com>
2026-05-25 19:45:47 +0700

    read data user

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -5,22 +5,28 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
+use App\Models\UserModel;
+
 class AuthController extends BaseController
 {
+    protected $userModel;
+
     function __construct()
     {
         helper('form');
+        $this->userModel = new UserModel();
     }
+
     public function login()
     {
         if ($this->request->getPost()) {
             $username = $this->request->getVar('username');
             $password = $this->request->getVar('password');
 
-            $dataUser = ['username' => 'Primas', 'password' => '202cb962ac59075b964b07152d234b70', 'role' => 'admin']; // passw 123
+            $dataUser = $this->userModel ->where(['username' => $username])->first();
 
-            if ($username == $dataUser['username']) {
-                if (md5($password) == $dataUser['password']) {
+            if ($dataUser) {
+	            if (password_verify($password, $dataUser['password'])) {
                     session()->set([
                         'username' => $dataUser['username'],
                         'role' => $dataUser['role'],

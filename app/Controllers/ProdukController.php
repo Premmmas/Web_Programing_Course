d204243ba2aecfdd586bf5978cccbb2e5db4e61d
Premmmas <pimiastianhi@gmail.com>
2026-06-08 10:16:49 +0700

    edit product controller

--- app/Controllers/ProdukController.php
+++ app/Controllers/ProdukController.php
@@ -5,8 +5,17 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
+use App\Models\ProductModel;
+
 class ProdukController extends BaseController
 {
+    protected $productModel; 
+
+    function __construct()
+    {
+        $this->productModel = new ProductModel();
+    }
+    
     public function index()
     {
         return view('v_produk');

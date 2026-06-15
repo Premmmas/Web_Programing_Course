d32b18661c7435d1d40d6bb2ae834f27d6c0d275
Premmmas <pimiastianhi@gmail.com>
2026-06-15 09:47:03 +0700

    Update delete function

--- app/Controllers/ProdukController.php
+++ app/Controllers/ProdukController.php
@@ -24,24 +24,61 @@ class ProdukController extends BaseController
         ]);
     }
     public function create()
-{
-    $dataFoto = $this->request->getFile('foto');
+    {
+        $dataFoto = $this->request->getFile('foto');
 
-    $dataForm = [
-        'nama' => $this->request->getPost('nama'),
-        'harga' => $this->request->getPost('harga'),
-        'jumlah' => $this->request->getPost('jumlah') 
-    ];
+        $dataForm = [
+            'nama' => $this->request->getPost('nama'),
+            'harga' => $this->request->getPost('harga'),
+            'jumlah' => $this->request->getPost('jumlah') 
+        ];
 
-    if ($dataFoto->isValid()) {
-        $fileName = $dataFoto->getRandomName(); 
-        $dataFoto->move('img/', $fileName);
-        
-        $dataForm['foto'] = $fileName;
+        if ($dataFoto->isValid()) {
+            $fileName = $dataFoto->getRandomName(); 
+            $dataFoto->move('img/', $fileName);
+            
+            $dataForm['foto'] = $fileName;
+        }
+
+        $this->productModel->insert($dataForm);
+
+        return redirect('produk')->with('success', 'Data Berhasil Ditambah');
+    }
+    public function edit($id)
+    {
+        $dataProduk = $this->productModel->find($id);
+
+        $dataForm = [
+            'nama' => $this->request->getPost('nama'),
+            'harga' => $this->request->getPost('harga'),
+            'jumlah' => $this->request->getPost('jumlah') 
+        ];
+
+        if ($this->request->getPost('check') == 1) {
+            if ($dataProduk['foto'] != '' and file_exists("img/" . $dataProduk['foto'] . "")) {
+                unlink("img/" . $dataProduk['foto']);
+            }
+
+            $dataFoto = $this->request->getFile('foto');
+
+            if ($dataFoto->isValid()) {
+                $fileName = $dataFoto->getRandomName();
+                $dataFoto->move('img/', $fileName);
+                
+                $dataForm['foto'] = $fileName;
+            }
+        }
+
+        $this->productModel->update($id, $dataForm);
+
+        return redirect('produk')->with('success', 'Data Berhasil Diubah');
     }
 
-    $this->productModel->insert($dataForm);
+    public function delete($id)
+    {
+        $dataProduk = $this->productModel->find($id);
+        $this->productModel->delete($id);
 
-    return redirect('produk')->with('success', 'Data Berhasil Ditambah');
-} 
+        return redirect('produk')->with('success', 'Data Berhasil Dihapus');
+    }
 }
